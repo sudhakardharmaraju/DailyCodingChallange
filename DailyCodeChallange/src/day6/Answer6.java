a7bb30d64df15e216ec96e28d9f199341af62084
Sudhakar Dharmaraju <sudhakar.dharmaraju@gmail.com>
2019-05-26 13:03:18 +0530

    #day7 Challange
    #day7 Challange # dynamic programming # rec

---
+++ DailyCodeChallange/src/day6/Answer6.java
@@ -0,0 +1,5 @@
+package day6;
+
+public class Answer6 {
+ // we cant access address in java still thinking how to implement
+}
---
+++ DailyCodeChallange/src/day7/Answer7.java
@@ -0,0 +1,38 @@
+package day7;
+
+public class Answer7 {
+	static int countDecodingDP(char digits[],  int n){
+		// A table to store results of subproblems 
+	    int count[] = new int[n + 1];  
+	    count[0] = 1; 
+	    count[1] = 1; 
+	  
+	    for (int i = 2; i <= n; i++) 
+	    { 
+	        count[i] = 0; 
+	  
+	        // If the last digit is not 0,  
+	        // then last digit must add to 
+	        // the number of words 
+	        if (digits[i - 1] > '0') 
+	            count[i] = count[i - 1]; 
+	  
+	        // If second last digit is smaller 
+	        // than 2 and last digit is smaller 
+	        // than 7, then last two digits  
+	        // form a valid character 
+	        if (digits[i - 2] == '1' || 
+	           (digits[i - 2] == '2' &&  
+	            digits[i - 1] < '7')) 
+	            count[i] += count[i - 2]; 
+	    } 
+	    return count[n]; 
+	} 
+	
+	public static void main (String[] args){  
+	   char digits[] = {'1','2', '1','2'}; 
+	    int n = digits.length; 
+	    System.out.println("Count is " +  
+	               countDecodingDP(digits, n)); 
+	} 
+}
